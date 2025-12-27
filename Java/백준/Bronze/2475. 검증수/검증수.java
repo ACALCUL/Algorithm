bbe0024ad38f6806059b7f00a50c00ccb0c225d7
Lee_Donghyun <105157783+ACALCUL@users.noreply.github.com>
2025-12-27 17:07:30 +0900

    [Bronze V] Title: 검증수, Time: 100 ms, Memory: 14132 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int result=0;
+        for(int i=0; i<5; i++){
+            int n = Integer.parseInt(st.nextToken());
+            result+=n*n;
+        }
+        result%=10;
+        System.out.println(result);
+    }
+}
