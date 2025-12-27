44089fc9898d23bddecf9be181fbda65adf6f889
Lee_Donghyun <105157783+ACALCUL@users.noreply.github.com>
2025-12-27 16:42:27 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 104 ms, Memory: 14096 KB -BaekjoonHub

--- Java/백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ Java/백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -5,15 +5,18 @@ import java.io.IOException;
 public class Main{
     public static void main(String[] args)  throws IOException{
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      int i = Integer.parseInt(br.readLine());
+      
+      int n = Integer.parseInt(br.readLine());
 
-      int k;
-      for(int j=1; j<i+1; j++){
-        for(k=0; k<j; k++){
-          System.out.print("*");
+      StringBuilder sb = new StringBuilder();
+
+      for(int i=1; i<=n; i++){
+        for(int j=0; j<i; j++){
+          sb.append("*");
         }
-        System.out.println("");
+        sb.append("\n");
       }
+      System.out.print(sb.toString());
 
     }
 }
