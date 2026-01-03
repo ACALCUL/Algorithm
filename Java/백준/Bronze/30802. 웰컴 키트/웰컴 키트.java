1ebba5401446ccc033a363683eba816606d92020
ACALCUL <105157783+ACALCUL@users.noreply.github.com>
2026-01-03 10:53:38 +0900

    [Bronze III] Title: 웰컴 키트, Time: 108 ms, Memory: 14588 KB -BaekjoonHub

--- Java/백준/Bronze/30802. 웰컴 키트/웰컴 키트.java
+++ Java/백준/Bronze/30802. 웰컴 키트/웰컴 키트.java
@@ -17,15 +17,19 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         int T = Integer.parseInt(st.nextToken());
         int P = Integer.parseInt(st.nextToken());
+        // 문제 조건에서 T, P의 범위가 2*10^9 이상이면 long 자료형 쓰기
 
         int T_NUM=0;
         int P_NUM=n/P;
         int P_MOD=n%P;
 
         for(int i=0; i<6; i++){
+            /*
             for(int j=0; j<size[i]; j+=T){
                 T_NUM++;
             }
+            */
+            T_NUM+=(T+size[i]-1)/T;
         }
         
         System.out.println(String.format("%d\n%d %d", T_NUM, P_NUM, P_MOD));
