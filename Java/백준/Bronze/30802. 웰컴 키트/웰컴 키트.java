0c69bb24c8d3aa8a8e7a4637c7a44857906d3daa
ACALCUL <105157783+ACALCUL@users.noreply.github.com>
2026-01-03 10:41:29 +0900

    [Bronze III] Title: 웰컴 키트, Time: 296 ms, Memory: 14432 KB -BaekjoonHub

---
+++ Java/백준/Bronze/30802. 웰컴 키트/웰컴 키트.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] size = new int[6];
+        for(int i=0; i<6; i++){
+            size[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int T = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken());
+
+        int T_NUM=0;
+        int P_NUM=n/P;
+        int P_MOD=n%P;
+
+        for(int i=0; i<6; i++){
+            for(int j=0; j<size[i]; j+=T){
+                T_NUM++;
+            }
+        }
+        
+        System.out.println(String.format("%d\n%d %d", T_NUM, P_NUM, P_MOD));
+
+    }
+}
