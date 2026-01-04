4942be894a4ded8d703d7febad0de6dc9787d315
ACALCUL <105157783+ACALCUL@users.noreply.github.com>
2026-01-04 15:30:15 +0900

    [Bronze I] Title: 이항 계수 1, Time: 104 ms, Memory: 14312 KB -BaekjoonHub

--- Java/백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ Java/백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -10,15 +10,11 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-        int result = 1;
+        double result = 1;
         for(int i=1; i<=k; i++){
-            result *= n;
-            n -= 1;
+            result = result*(n-i+1)/i;
         }
-        for(int i=2; i<=k; i++){
-            result /= i;
-        }
-        System.out.println(result);
+        System.out.println((int)(result));
     }
     
 }
