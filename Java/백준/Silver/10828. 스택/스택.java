6bfe686cc847390a21689cf2e29f4ff91af8c17f
ACALCUL <105157783+ACALCUL@users.noreply.github.com>
2026-01-10 10:02:30 +0900

    [Silver IV] Title: 스택, Time: 160 ms, Memory: 19572 KB -BaekjoonHub

---
+++ Java/백준/Silver/10828. 스택/스택.java
@@ -0,0 +1,77 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static class Stack{
+        int top=-1;
+        int[] array = new int[10000];
+
+        void push(int num){
+            if(top>=10000){
+                return;
+            }
+            array[++top]=num;
+        }
+        int pop(){
+            if(empty()==1){
+                return -1;
+            }
+            return array[top--];
+        }
+        int size(){
+            return top+1;
+        }
+        int empty(){
+            return (top==-1)?1:0;
+        }
+        int top(){
+            if(top<0){
+                return -1;
+            }
+            return array[top];
+        }
+
+    }
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        String s;
+        // 객체는 매개변수로 쓰일 시에 주솟값 복사하므로 객체의 변수 변경 가능
+        Stack stack = new Stack(); 
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0; i<n; i++){
+            s=br.readLine();
+            switch(s){ // 분기 상태가 한정적이면 switch가 성능이 훨씬 좋음
+                case "pop":
+                    sb.append(stack.pop()).append("\n");
+                    break;
+                case "size":
+                    sb.append(stack.size()).append("\n");
+                    break;
+                case "empty":
+                    sb.append(stack.empty()).append("\n");
+                    break;
+                case "top":
+                    sb.append(stack.top()).append("\n");
+                    break;
+                default:
+                    if(s.substring(0,5).equals("push ")){
+                        // substring(a,b): a이상, b미만 인덱스를 리턴
+                        st=new StringTokenizer(s);
+                        st.nextToken();
+                        stack.push(Integer.parseInt(st.nextToken()));
+                    }
+                    break;
+            }
+
+        }
+
+        System.out.print(sb.toString());
+
+    }
+
+}
