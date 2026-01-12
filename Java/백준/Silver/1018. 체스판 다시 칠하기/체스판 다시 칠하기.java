17dc6f5c3aeac41c7418d8fe00549bb986d2bb92
ACALCUL <105157783+ACALCUL@users.noreply.github.com>
2026-01-12 15:09:45 +0900

    [Silver III] Title: 체스판 다시 칠하기, Time: 108 ms, Memory: 14304 KB -BaekjoonHub

--- Java/백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ Java/백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -18,8 +18,8 @@ public class Main {
         int minColorCount=64;
         int colorCount;
 
-        for(int i=0; i<n-8+1; i++){
-            for(int j=0; j<m-8+1; j++){
+        for(int i=0; i<=n-8; i++){
+            for(int j=0; j<=m-8; j++){
                 colorCount=countColor(board, i, j);
                 minColorCount=(minColorCount<colorCount)?minColorCount: colorCount;
             }
@@ -29,39 +29,24 @@ public class Main {
     }
 
     private static int countColor(char[][] board, int start_x, int start_y){
-        int colorCount1=0;
-        int colorCount2=0;
+        int colorCount=0;
         int adjustmentAmount=0;
-        char startColor=board[start_x][start_y];
+        char startColor='W';
         for(int i=start_x; i<start_x+8; i++){
             for(int j=start_y; j<start_y+8; j++){ // 8*8만 봄
                 if((i+j+adjustmentAmount)%2==0){ // start_color와 다르면 count++
                     if(startColor!=board[i][j]){
-                        colorCount1++;
+                        colorCount++;
                     }
                 } else{ // start_color와 같으면 count++
                     if(startColor == board[i][j]){
-                        colorCount1++;
+                        colorCount++;
                     }
                 }
             }
         }
-        adjustmentAmount=1;
-        for(int i=start_x; i<start_x+8; i++){
-            for(int j=start_y; j<start_y+8; j++){ // 8*8만 봄
-                if((i+j+adjustmentAmount)%2==0){ // start_color와 다르면 count++
-                    if(startColor!=board[i][j]){
-                        colorCount2++;
-                    }
-                } else{ // start_color와 같으면 count++
-                    if(startColor == board[i][j]){
-                        colorCount2++;
-                    }
-                }
-            }
-        }
-        colorCount1=(colorCount1<colorCount2)?colorCount1:colorCount2;
-        return colorCount1;
+        // 'B'일 경우의 colorCount는 64-colorCount
+        return (colorCount<64-colorCount)?colorCount:64-colorCount;
     }
 
 }
