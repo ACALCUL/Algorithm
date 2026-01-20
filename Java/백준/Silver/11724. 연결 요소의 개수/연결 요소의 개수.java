563e85b72c966244f25fc1bf7c4567e7d231757b
ACALCUL <105157783+ACALCUL@users.noreply.github.com>
2026-01-20 10:24:29 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 576 ms, Memory: 142328 KB -BaekjoonHub

---
+++ Java/백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        for(int i=0; i<=n; i++){
+            graph.add(new ArrayList<Integer>());
+        }
+        for(int i=0; i<m; i++){
+            st= new StringTokenizer(br.readLine());
+            int v = Integer.parseInt(st.nextToken());
+            int u = Integer.parseInt(st.nextToken());
+            graph.get(v).add(u);
+            graph.get(u).add(v);
+        }
+
+        // for(int i=0; i<=n; i++){
+        //     System.out.println(graph.get(i));
+        // }
+        System.out.println(countConnect(n, graph));
+    }
+
+    private static int countConnect(int n, ArrayList<ArrayList<Integer>> graph){
+        int count=0;
+        boolean[] visited = new boolean[n+1];
+        Queue<Integer> queue = new ArrayDeque<>();
+        
+        for(int i=1; i<=n; i++){
+            if(visited[i]){ continue; }
+
+            queue.add(i);
+            visited[i]=true;
+            count++;
+
+            while(!queue.isEmpty()){
+                for(int next: graph.get(queue.poll())){
+                    if(!visited[next]){
+                        queue.add(next);
+                        visited[next]=true;
+                    }
+                }
+            }
+
+        }
+        return count;
+    }
+}
