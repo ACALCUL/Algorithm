9369b58f6b87cfe0a246ede931e74ac6a36eb6b5
ACALCUL <105157783+ACALCUL@users.noreply.github.com>
2026-02-11 14:50:12 +0900

    [Bronze II] Title: 분해합, Time: 88 ms, Memory: 14028 KB -BaekjoonHub

--- Java/백준/Bronze/2231. 분해합/분해합.java
+++ Java/백준/Bronze/2231. 분해합/분해합.java
@@ -1,35 +1,32 @@
 import java.io.*;
-import java.util.*;
 
 public class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int numDigit=0;
-        int targetNum=0;
+        System.out.println(solve(n));
+    }
 
+    private static int solve(int n){
+        int numDigit=0;
         for(int i=1; n/i!=0; i*=10){
             numDigit++;
         }
         // System.out.println(numDigit);
 
-        for(int i=n-9*numDigit; i<=n; i++){
-            // System.out.println("i: "+ i);
-            int count = i;
-            int k= 10;
-            for(int j=0; j<numDigit; j++){
-                int digitNum = i%k/(k/10);
+        for(int curNum=n-9*numDigit; curNum<=n; curNum++){
+            // System.out.println("curNum: "+ curNum);
+            int count = curNum;
+            for(int i = 1; curNum/i!=0; i*=10){
+                int digitNum = curNum/i%10;
                 // System.out.println(digitNum);
                 count+=digitNum;
-                k*=10;
             }
-            // sSystem.out.println("count: "+count);
+            // System.out.println("count: "+count);
             if(count == n){
-                targetNum = i;
-                break;
+                return curNum;
             }
         }
-        System.out.println(targetNum);
-        
+        return 0;
     }
 }
