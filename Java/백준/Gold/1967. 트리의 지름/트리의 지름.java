306ed2205a3ba09fe58ef646ee89fcb9bbb0627a
ACALCUL <105157783+ACALCUL@users.noreply.github.com>
2026-02-12 12:27:47 +0900

    [Gold IV] Title: 트리의 지름, Time: 216 ms, Memory: 22876 KB -BaekjoonHub

--- Java/백준/Gold/1967. 트리의 지름/트리의 지름.java
+++ Java/백준/Gold/1967. 트리의 지름/트리의 지름.java
@@ -3,8 +3,8 @@ import java.util.*;
 
 public class Main {
     private static class Node{
-        List<Integer> adj = new ArrayList<>();
-        List<Integer> weight = new ArrayList<>();
+        List<int[]> edges = new ArrayList<>();
+        // adjEdge는 (node, weight) 형태
     }
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,28 +18,25 @@ public class Main {
         String line;
 
         while((line=br.readLine()) != null){
+            if(line.isEmpty()) break;
             st = new StringTokenizer(line);
             int parent = Integer.parseInt(st.nextToken());
             int child = Integer.parseInt(st.nextToken());
             int weight = Integer.parseInt(st.nextToken());
-            nodes[parent].adj.add(child);
-            nodes[parent].weight.add(weight);
-
-            nodes[child].adj.add(parent);
-            nodes[child].weight.add(weight);
+            nodes[parent].edges.add(new int[]{child, weight});
+            nodes[child].edges.add(new int[]{parent, weight});
         }
 
         // for(int i=1; i<=nodeNum; i++){ // for printing tree
         //     Node node = nodes[i];
-        //     System.out.print("i:"+i+"-> ");
-        //     for(int j=0; j<node.adj.size(); j++){
-        //         System.out.print(node.adj.get(j)+","+node.weight.get(j)+" ");
+        //     System.out.print("node: "+i+"-> ");
+        //     for(int[] edge: node.edges){
+        //         System.out.print(edge[0]+","+edge[1]+" ");
         //     }
         //     System.out.println();
         // }
 
         int startIndex = findMaxWeight(nodes, nodeNum, 1)[1];
-        // System.out.println(startIndex); // for checking startIndex
 
         // 아무 점과 제일 먼 노드는 지름의 두 끝 노드 중 하나
         // 왜냐하면 지름의 정의가 제일 먼 두 노드의 거리를 의미하고, 
@@ -49,41 +46,34 @@ public class Main {
     }
 
     private static int[] findMaxWeight(Node[] nodes, int nodeNum, int startIndex){
-        if(nodeNum < 2){
-            return new int[]{0, 0};
-        }
-
+        //System.out.println("\nstartIndex: "+startIndex);
         int maxWeight = 0; // startNode에서 제일 먼 Node까지의 weight
-        int maxWeightIndex = 0; // result[1]: startNode에서 제일 먼 Node의 인덱스
-        int[] weights= new int[nodeNum+1];
+        int maxWeightIndex = startIndex; // startNode에서 제일 먼 Node의 인덱스
         boolean[] visited = new boolean[nodeNum+1];
 
         Deque<int[]> stack = new ArrayDeque<>();
-        // stack에 들어갈 노드: (Index, depth, weight)
-        stack.push(new int[]{startIndex, 0, 0});
+        // stack에 들어갈 노드: (Index, weight)
+        stack.push(new int[]{startIndex, 0});
+        visited[startIndex] = true;
+
         while(!stack.isEmpty()){
             int[] element = stack.pop();
             int curIndex = element[0];
-            Node curNode = nodes[curIndex];
-            int curDepth = element[1];
-            int curWeight = element[2];
+            int curWeight = element[1];
 
-            visited[curIndex]= true;
-            weights[curDepth]=curWeight;
-
-            int sum = sumWeights(weights, curDepth);
-            for(int i=0; i<curNode.adj.size(); i++){
-                int nextIndex = curNode.adj.get(i);
+            for(int[] edge: nodes[curIndex].edges){
+                int nextIndex = edge[0];
                 if(visited[nextIndex]) { continue; }
-                int nextWeight = curNode.weight.get(i);
+                int nextWeight = curWeight + edge[1];
 
-                if(sum+nextWeight > maxWeight){
-                    maxWeight = sum+nextWeight;
+                if(nextWeight > maxWeight){
+                    maxWeight = nextWeight;
                     maxWeightIndex = nextIndex;
                 }
 
-                stack.push(new int[]{nextIndex, curDepth+1, nextWeight});
-                // System.out.println(String.format("put ( %d, %d, %d )",nextIndex, curDepth+1, nextWeight));
+                stack.push(new int[]{nextIndex, nextWeight});
+                visited[nextIndex]=true;
+                //System.out.println(String.format("put ( %d, %d )", nextIndex, nextWeight));
             }
             
         }
@@ -91,11 +81,4 @@ public class Main {
         return new int[]{maxWeight, maxWeightIndex};
     }
 
-    private static int sumWeights(int[] weights, int index){ // index이하의 weights의 총합
-        int sum = 0;
-        for(int i=0; i<=index; i++){
-            sum += weights[i];
-        }
-        return sum;
-    }
 }
