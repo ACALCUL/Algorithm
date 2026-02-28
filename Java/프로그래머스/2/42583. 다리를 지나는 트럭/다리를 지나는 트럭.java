8b2f6fa4835358e99737a9670788f6afd3b027c3
ACALCUL <105157783+ACALCUL@users.noreply.github.com>
2026-02-28 22:04:08 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.80 ms, Memory: 85.5 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int time = 0;
+        int trucksWeight = 0; // Weight of trucks that crosses the bridge
+        int[] startCrossingTime = new int[truck_weights.length]; // Time that the truck starts crossing the bridge
+        Queue<Integer> crossingTrucks= new ArrayDeque<>();
+        
+        for(int i=0; i<truck_weights.length; i++){
+            int truck_weight = truck_weights[i];
+            while(trucksWeight+truck_weight > weight){ // while weight of trucks is bigger than max weight
+                time = Math.max(time, startCrossingTime[crossingTrucks.peek()]+bridge_length);
+                trucksWeight-=truck_weights[crossingTrucks.poll()];
+                
+                
+            }
+            crossingTrucks.offer(i);
+            trucksWeight += truck_weight;
+            startCrossingTime[i] = time++;
+        }
+        int finalTruck=-1;
+        while(!crossingTrucks.isEmpty()){
+            finalTruck = crossingTrucks.poll();
+        }
+        time=startCrossingTime[finalTruck]+bridge_length+1;
+        
+        return time;
+    }
+}
