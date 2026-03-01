f943a26cca1e2b24a031e172f44e9ee08510cdd6
ACALCUL <105157783+ACALCUL@users.noreply.github.com>
2026-03-01 15:04:50 +0900

    [level 2] Title: 주식가격, Time: 31.30 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        
+    int[] answer = new int[prices.length];
+    Deque<Integer> stack = new ArrayDeque<>();
+    for(int i=0; i<prices.length; i++){
+        while(!stack.isEmpty() && prices[i] < prices[stack.peek()]){
+            int index = stack.poll();
+            answer[index]= i-index;
+        }
+        stack.push(i);
+    }
+    while(!stack.isEmpty()){
+        int index = stack.poll();
+        answer[index] = prices.length-1-index;
+    }
+    
+    return answer;
+    }
+}
