c401643e06962272fdf718cb9be571c6ed4653f8
ACALCUL <105157783+ACALCUL@users.noreply.github.com>
2026-03-07 10:15:16 +0900

    [level 2] Title: 소수 찾기, Time: 25.26 ms, Memory: 92.2 MB -BaekjoonHub

--- Java/프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ Java/프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -33,9 +33,7 @@ class Solution {
         if(num==2){ return true; }
         if(num<2 || num%2 == 0){ return false; }
         for(int i=3; i*i<=num; i+=2){
-            if(num%i==0){
-                return false;
-            }
+            if(num%i==0){ return false; }
         }
         return true;
     }
