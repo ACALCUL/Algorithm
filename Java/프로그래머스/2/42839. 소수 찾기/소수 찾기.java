e2a270da4cf26ad9e57eed9524e2132627cea730
ACALCUL <105157783+ACALCUL@users.noreply.github.com>
2026-03-07 10:14:11 +0900

    [level 2] Title: 소수 찾기, Time: 22.11 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Solution {
+    
+    public int solution(String numbers) {
+        HashSet<Integer> set = new HashSet<>();
+        boolean[] visited = new boolean[numbers.length()];
+        getResultNum(numbers, set, visited, "");
+        
+        int answer = 0;
+        for(int num: set){
+            if(isPrime(num)){ answer++; }
+        }
+        return answer;
+    }
+    
+    private void getResultNum(String numbers, HashSet<Integer> set, boolean[] visited, String resultNum){
+        if(!resultNum.equals("")){
+            set.add(Integer.parseInt(resultNum));
+        }
+        
+        for(int i=0; i<numbers.length(); i++){
+            if(!visited[i]){
+                visited[i]= true;
+                getResultNum(numbers, set, visited, resultNum+numbers.charAt(i));
+                visited[i]= false;
+            }
+            
+        }
+    }
+    
+    private boolean isPrime(int num){
+        if(num==2){ return true; }
+        if(num<2 || num%2 == 0){ return false; }
+        for(int i=3; i*i<=num; i+=2){
+            if(num%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
