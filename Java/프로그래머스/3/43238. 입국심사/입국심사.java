fb4ec35986672057d0f27ae7bb3caa141cd3b3b2
ACALCUL <105157783+ACALCUL@users.noreply.github.com>
2026-03-15 14:39:22 +0900

    [level 3] Title: 입국심사, Time: 59.16 ms, Memory: 94.4 MB -BaekjoonHub

--- Java/프로그래머스/3/43238. 입국심사/입국심사.java
+++ Java/프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,6 +1,28 @@
+import java.util.*;
+
 class Solution {
     public long solution(int n, int[] times) {
-        long answer = 0;
-        return answer;
+        // low~high 중 n<=sigma(T/times[i])를 만족하는 T의 최솟값 구하기
+        // -> 결정문제 -> LowerBound 구하기
+        long low = 1;
+        long high = 1;
+        for(int i=0; i<times.length; i++){
+            high = Math.max(high, times[i]);
+        }
+        high *= n;
+        while(low<high){
+            long mid = low+(high-low)/2;
+            long finishedNum = 0;
+            for(int i=0; i<times.length; i++){
+                finishedNum +=mid/times[i];
+            }
+            // true인 조건: finishedNum >= n
+            if(finishedNum < n){
+                low =mid+1;
+            }else{
+                high = mid;
+            }
+        }
+        return high;
     }
 }
