3679bf29970ad4206393b3a611e39b3a5b08a19a
ACALCUL <105157783+ACALCUL@users.noreply.github.com>
2026-03-28 16:15:11 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 85.4 MB -BaekjoonHub

--- Java/프로그래머스/2/42842. 카펫/카펫.java
+++ Java/프로그래머스/2/42842. 카펫/카펫.java
@@ -2,13 +2,16 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int brown, int yellow) {
-        // yellow의 모든 width, height 경우의 수를 구하고
-        // brown의 값에 맞으면 brown의 가로, 세로를 반환
         for(int i=1; i*i<=yellow; i++){
             if(yellow % i == 0){
-                int width = Math.max(i, yellow/i);
-                int height = Math.min(i, yellow/i);
+                // yellow의 가로: width, 세로: heigth
+                int width = yellow/i;
+                // i*i<=yellow이므로, yellow/i가 항상 i보다 큼
+                int height = i;
+                
+                // brown의 값과 같은지 검증
                 if(brown == 2*(width+height)+4){
+                    //brown의 가로, 세로 리턴
                     return new int[]{width+2, height+2};
                 }
             }
