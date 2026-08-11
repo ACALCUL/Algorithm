5ef01ca6555ce1b6bead3e4058f2c6ab00229df5
Donghyun Lee <105157783+ACALCUL@users.noreply.github.com>
2026-08-11 11:25:44 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 64.73 ms, Memory: 124 MB -BaekjoonHub

--- Java/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ Java/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,26 +2,26 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        
         Map<String, Integer> map = new HashMap<>();
-        for(String person: participant){
-            map.put(person, map.getOrDefault(person, 0)+1);
-        }
-        
-        // for(String key: map.keySet()){
-        //     System.out.println("key: "+key+" value:"+map.get(key));
-        // }
-        
-        for(String person: completion){
-            map.put(person, map.getOrDefault(person, 0)-1);
-        }
+        for(String person: participant){ map.put(person, map.getOrDefault(person, 0)+1); }
+        // for(String key: map.keySet()){ System.out.println("key: "+key+" value:"+map.get(key)); }
+        for(String person: completion){ map.put(person, map.getOrDefault(person, 0)-1); }
         
         for(String key: map.keySet()){
-            if(map.get(key)>=1){
-                return key;
+            if(map.get(key)!=0){
+                answer=key;
             }
         }
         
-        String answer = "";
+        // for(Map.Entry<String,Integer> entry: map.entrySet()){
+        //     if(entry.getValue()!=0){
+        //         answer= entry.getKey();
+        //     }
+        // }
+        
+        
         return answer;
     }
 }
