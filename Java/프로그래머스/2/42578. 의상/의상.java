bd70cc87b2ff720fb3ecdf4fef2afded0bd7dfc2
Donghyun Lee <105157783+ACALCUL@users.noreply.github.com>
2026-08-22 22:11:20 +0900

    [level 2] Title: 의상, Time: 1.44 ms, Memory: 75 MB -BaekjoonHub

--- Java/프로그래머스/2/42578. 의상/의상.java
+++ Java/프로그래머스/2/42578. 의상/의상.java
@@ -1,27 +1,19 @@
 import java.util.*;
 
-class Solution {
-    public int solution(String[][] clothes) {
-        HashMap<String, Integer> typeIdToNum = new HashMap<>();
-        for(String[] element: clothes){
-            String type = element[1];
-            typeIdToNum.merge(type, 1, Integer::sum);
-        }
-        // merge 메소드란?
-        // 첫번째 인자: myKey, 두 번째 인자: myValue, 세번째 인자: remapping function
-        // myKey가 HashMap에 없다면, (myKey, myValue)를 집어넣는다
-        // myKey가 HashMap에 있다면, 기존 value와 myValue를 각각 1, 2번째 인자로 지정한 remapping function의 반환값을 myKey의 value로 대체한다.
-            
-        // Integer::sum의 의미:
-        // (a, b) -> Integer.sum(a,b);
+class Solution{
+    public int solution(String[][] clothes){
+        HashMap<String, Integer> map = new HashMap<>();
         
-        int answer = 1;
-        for(String type: typeIdToNum.keySet()){
-            // keySet(): 모든 key를 Set 객체로 변환(중복값이 허용되지 않으므로 Set)
-            // values(): 모든 value를 Collection 객체로 변환
-            answer*=typeIdToNum.get(type)+1;
+        for(String[] clothe: clothes){
+            map.put(clothe[1], map.getOrDefault(clothe[1], 0)+1);
         }
         
-        return --answer;
+        int num=1;
+        for(int value: map.values()){
+            System.out.println(value);
+            num*=value+1;
+        }
+        
+        return num-1;
     }
 }
