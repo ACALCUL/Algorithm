e09d165a8862988444b5b2b89c021a1779e1f488
Donghyun Lee <105157783+ACALCUL@users.noreply.github.com>
2026-08-24 23:28:03 +0900

    [level 3] Title: 베스트앨범, Time: 2.23 ms, Memory: 81.9 MB -BaekjoonHub

--- Java/프로그래머스/3/42579. 베스트앨범/README.md
+++ Java/프로그래머스/3/42579. 베스트앨범/README.md
@@ -1,10 +1,10 @@
 # [level 3] 베스트앨범 - 42579 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579) 
 
 ### 성능 요약
 
-메모리: 78.5 MB, 시간: 7.58 ms
+메모리: 81.9 MB, 시간: 2.23 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 26일 23:12:29
+2026년 08월 24일 23:27:59
 
 ### 문제 설명
 
--- Java/프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ Java/프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -3,49 +3,59 @@ import java.util.*;
 
 
 class Solution {
-    
-    class Song implements Comparable<Song>{
+    class Song{
         int id;
-        long play;
-        
-        Song(int id, long play){
+        int play;
+        Song(int id, int play){
             this.id = id;
             this.play = play;
         }
-        
-        @Override
-        public int compareTo(Song song){
-            if(this.play == song.play) return Integer.compare(this.id, song.id); // 오름차순
-            return Long.compare(song.play, this.play); // 내림차순
-        }
     }
     
-    
     public int[] solution(String[] genres, int[] plays) {
-        HashMap<String, Long> genrePlayCount = new HashMap<>();
-        HashMap<String, List<Song>> songsByGenre = new HashMap<>();
-        for(int i=0; i<genres.length; i++){
-            genrePlayCount.put(genres[i], genrePlayCount.getOrDefault(genres[i], 0L)+plays[i]);
-            songsByGenre.putIfAbsent(genres[i], new ArrayList<>());
-            songsByGenre.get(genres[i]).add(new Song(i, plays[i]));
+        
+        Map<String, Integer> playCountByGenre = new HashMap<>();
+        Map<String, List> songByGenre = new HashMap<>();
+        
+        for(int i=0 ; i<genres.length; i++){
+            playCountByGenre.put(genres[i], 
+                                 playCountByGenre.getOrDefault(genres[i], 0)+plays[i]);
+            songByGenre.putIfAbsent(genres[i], new ArrayList<Song>());
+            songByGenre.get(genres[i]).add(new Song(i, plays[i]));
         }
         
-        List<String> sortedGenres = new ArrayList<>(genrePlayCount.keySet());
-        sortedGenres.sort((a,b) -> { return Long.compare(genrePlayCount.get(b), genrePlayCount.get(a)); });
-        // Collections.sort(sortedGenres, (a,b) -> ... ) 으로 대체 가능
+        List<String> genreOrder = new ArrayList<>(playCountByGenre.keySet());
+        Collections.sort(genreOrder, (a, b) -> 
+                  { return -Integer.compare(playCountByGenre.get(a), playCountByGenre.get(b)); });
+        
+        // for(String genre: genreOrder){
+        //     System.out.printf("Genre: %s, Play: %d\n", genre, playCountByGenre.get(genre));
+        // }
         
         List<Integer> answerList = new ArrayList<>();
-        for(String genre: sortedGenres){
-            List<Song> songs = songsByGenre.get(genre);
-            songs.sort(null); // Compartable<Song> 인터페이스의 compareTo를 override했으므로 정렬함수를 인자로 넣지 않아도 됨
-            // Collections.sort(songs); 으로 대체 가능
-            for(int i=0; i<Math.min(songs.size(), 2); i++){
-                answerList.add(songs.get(i).id);
+        
+        for(String genre: genreOrder){
+            List<Song> songList = songByGenre.get(genre);
+            songList.sort((a, b)->{
+                if(a.play == b.play){ 
+                    return Integer.compare(a.id, b.id); 
+                }else{
+                    return -Integer.compare(a.play, b.play);
+                }
+            });
+            for(int i=0; i<Math.min(2, songList.size()); i++){
+                
+                answerList.add(songList.get(i).id);
+                //System.out.println(songList.get(i).id);
             }
-            
         }
         
-        return answerList.stream().mapToInt(Integer::intValue).toArray();
-        // mapToInt(Integer::intValue) 대신 mapToInt(i->i)로 대체 가능
+        
+        int[] answer = new int[answerList.size()];
+        for(int i=0; i<answerList.size(); i++){
+            answer[i]=answerList.get(i);
+        }
+        
+        return answer;
     }
 }
