fa8eb6102b6a9b1fa774a2a7fcdd6afcc8340a73
Donghyun Lee <105157783+ACALCUL@users.noreply.github.com>
2026-09-03 15:26:43 +0900

    [level 2] Title: 올바른 괄호, Time: 10.05 ms, Memory: 58.5 MB -BaekjoonHub

--- Java/프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ Java/프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,19 +2,20 @@ import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        Deque<Character> stack = new ArrayDeque<>();
+        Deque<Integer> stack = new ArrayDeque<>();
+        
         for(char c: s.toCharArray()){
             if(c=='('){
-                stack.push('(');
-            }else if(c==')'){
+                stack.push(1);
+            }else{ // ')'일 경우
                 if(stack.isEmpty()){
                     return false;
-                } else{
+                }else{
                     stack.pop();
                 }
             }
         }
-
+        
         return stack.isEmpty()?true:false;
     }
 }
